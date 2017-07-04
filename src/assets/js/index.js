75d128e99a15cf2e02c5093b96ce3dba2a0f0801
maryleo3007 <osmary.vega@gmail.com>
2017-07-04 16:56:10 -0500

    add modal

--- src/assets/js/index.js
+++ src/assets/js/index.js
@@ -10,11 +10,9 @@ const render = (root) => {
   });
   root.append(wrapper);
 }
-
 const state = {
     pins: null
 }
-
 $(_ => {
   getJSON('https://api.pinterest.com/v1/boards/arabelyuska/web-ui/pins/?access_token=AR1ezCPQvOjgIniGPFyboS_ooLgIFM42OdzL6V9EI3AfhAAp8AAAAAA&fields=id%2Clink%2Cnote%2Curl%2Cattribution%2Cboard%2Ccolor%2Ccounts%2Ccreated_at%2Ccreator%2Cimage%2Cmedia%2Cmetadata%2Coriginal_link',(err,json) =>{
     if (err) {return alert(err.message);}
