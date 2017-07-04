9e0b8e405860166da08660087b67aae6c4a9caec
Janine Vega <osmary.vega@gmail.com>
2017-07-04 09:48:28 -0500

    add gulp strcture

---
+++ server.js
@@ -0,0 +1,9 @@
+const express = require('express'); //Framework Express
+
+const app = express();
+
+app.use('/',express.static('public'));
+
+app.listen(3000, () => {
+  console.log('Server running on port 3000!');
+});
