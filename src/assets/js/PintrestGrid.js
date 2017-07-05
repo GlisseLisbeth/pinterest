b6aeef985805b85ad6dc1bfc0b20f3ff00e69a08
glisselisbeth <glisselisbeth@gmail.com>
2017-07-05 17:39:43 -0500

    add grid

--- src/assets/js/PintrestGrid.js
+++ src/assets/js/PintrestGrid.js
@@ -1,28 +1,19 @@
 'use strict';
 
 const BoardItem = (pinterest,update) => {
-
-  const colcontainer = $('<div class="col-md-3"></div>');
-  const divcontentPin = $('<div class="contentPin"></div>');
-  const figure = $('<div class="figPin"></div>');
+  console.log(pinterest);
+  const figure = $('<div class="pin"></div>');
   const a = $('<a data-toggle="modal" data-target="#myModal"></a>');
 
   const img = $('<img class="pinimg center" src="'+pinterest.image.original.url+'">');
+  const p = $('<p>'+pinterest.note+'</p>');
 
   figure.append(a);
+  figure.append(p);
   a.append(img);
 
-  const divnmbre = $('<div class="containername col-md-12"></div>');
 
-  //divnmbre.append(pcolection);
-
-  colcontainer.append(divcontentPin);
-  divcontentPin.append(figure);
-
-  colcontainer.append(divnmbre);
-  divnmbre.append(PinDetails());
-
-  let doublePin = colcontainer.map((x)=>{ return x});
+  let doublePin = figure.map((x)=>{ return x});
   // a.on('click',function () {
   //
   //   $.get(,(data) => {
@@ -32,23 +23,24 @@ const BoardItem = (pinterest,update) => {
   // });
 
 
-return colcontainer;
+return figure;
 }
 
 const BoardGrid = (update) => {
 
   const section = $('<section id ="sectionPin"></section>');
-  const container  = $('<div class="container"></div>');
-  const rowgrid = $('<div class="row"></div>');
-  const board = $('<div class="board"></div>');
-
+  const container  = $('<div class="container-fluid" style="background:red"></div>');
+  const rowgrid = $('<div class="row" style="background:yellow"></div>');
+  const board = $('<div id="wrapper" class="board col-md-10 col-md-offset-1" col-xs-2 style="background:black"></div>');
+  const columns = $('<div id="columns"></div>');
   section.append(container);
   container.append(rowgrid);
   rowgrid.append(board);
+  board.append(columns);
   // reRender(rowgrid,filterpinterest(state.pinterests,""));
   const list = state.pins;
   list.forEach((pin) => {
-    board.append(BoardItem(pin));
+    columns.append(BoardItem(pin));
   });
 
   return section;
